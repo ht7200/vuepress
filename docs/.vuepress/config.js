2334693091d8896ce8705adf5696d42abce027e4
hongtun <hongtun@ahyzyx.cn>
2019-05-07 09:39:30 +0800

    icons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Great Front-End Documents',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
-    ['link', { rel: 'icon', href: 'http://www.chinayzyx.cn/images/yzyx.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/icons/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/', // 这是部署到github相关的配置 下面会讲
   markdown: {
