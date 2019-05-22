76400cebc1459f09480651f0f4f6c229c2155b05
hongtun <hongtun@ahyzyx.cn>
2019-05-22 16:23:14 +0800

    切换发布分支

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f http://10.10.10.45:8000/ht7200/FE-DOC.git master:gh-pages
+git push -f http://10.10.10.45:8000/PL/FE-DOC.git master:master
 
 cd ../../..
